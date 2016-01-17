444911d3f728093fedd9314808c7306297a4637b
Hector Otero <otero111@hotmail.com>
2016-01-16 23:15:17 -0800

    added more functionality

---
+++ SoccerRefereeApp/SurfJournal/ScoreViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ScoreViewController.swift
+//  SurfJournal
+//
+//  Created by Hector Otero on 1/16/16.
+//  Copyright © 2016 Razeware. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import CoreData
+
+class ScoreViewController: UIViewController {
+    
+}
